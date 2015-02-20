a1a52cbd0aa5b258b74a20b2f77166141aebf225
Peter Merek <ptmerek@sbcglobal.net>
2015-02-20 14:43:53 -0800

    updates
    update final cdv

--- run_analysis.R
+++ run_analysis.R
@@ -12,22 +12,20 @@ library(reshape2) ## melting df
 
 setwd("/Users/petermerek/Desktop/Coursera/CleanData/Cleanprog")     
 
-## download data in compressed format; decompress
+
+## Collect data, download data in compressed format; decompress
 url<-("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")
 download.file(url, destfile="HARData.zip", method="curl")
 unzip("/Users/petermerek/Desktop/Coursera/CleanData/Cleanprog/HARData.zip")
 
-## create vector of feature and activity names; descriptive names for tasks 3 and 4
+## Create metadata and descriptive column and variable names
 features<-read.delim("/Users/petermerek/Desktop/Coursera/CleanData/Cleanprog/UCI HAR Dataset/features.txt",sep="", header=FALSE)
-colnames(features)<-c("feature_num", "feature")
-
 activity_labels<-read.delim("/Users/petermerek/Desktop/Coursera/CleanData/Cleanprog/UCI HAR Dataset/activity_labels.txt",sep="",header=FALSE)
+colnames(features)<-c("feature_num", "feature")
 colnames(activity_labels)<-c("activity_num", "activity")
+fnames<-as.vector(features[,2])  ## variable names from features
 
-fnames<-as.vector(features[,2])
-
-
-## read in and tidy test data (test_subject, test_x and test_y)
+## Read in and tidy test data (test_subject, test_x and test_y)
 
 test_subject<-read.delim("/Users/petermerek/Desktop/Coursera/CleanData/Cleanprog/UCI HAR Dataset/test/subject_test.txt",sep="", header=FALSE)
 colnames(test_subject)<-c("subject_num")
@@ -42,10 +40,10 @@ colnames(test_x)<-c(fnames) ## add descriptive names to variables Item 4 in grad
 
 testM<-cbind(test,test_x) 
 
-test1 <- merge(testM, activity_labels, by="activity_num") ## add descriptive names to activities Item 3 in grading
+test1 <- merge(testM, activity_labels, by="activity_num") 
  
 
-## read in and tidy training data (train_subject, train_x, train_y)
+## Read in and tidy training data (train_subject, train_x, train_y)
 
 train_subject<-read.delim("/Users/petermerek/Desktop/Coursera/CleanData/Cleanprog/UCI HAR Dataset/train/subject_train.txt",sep="", header=FALSE)
 colnames(train_subject)<-c("subject_num")
@@ -60,28 +58,27 @@ colnames(train_x)<-c(fnames) ## add descriptive names to variables, item 4 in gr
 
 trainM<-cbind(train,train_x) ## common row order, column combine works
 
-train1 <- merge(trainM, activity_labels, by="activity_num") ## add descriptive name to activity, item 3 in grading
+train1 <- merge(trainM, activity_labels, by="activity_num") 
 
 
-## Merge train and test data sets,  "melt" to long form - item 1 in grading
+## Merge train and test data sets,  "melt" to long form 
 data<-rbind(train1,test1)
-data1 <- melt(data, id=c("activity_num", "subject_num","activity"))
+data1 <- melt(data, id=c("activity_num", "subject_num","activity")
+colnames(data1)<-c("activity_num","subject_num","activity","measurement","value") ## add descriptive names to variables, item 4 in grading 
 
-## Extract mean and std dev only, create new tidy data set  Items 2 and 5 in grading 
+## Extract mean and std dev only, create new tidy data set  
 data2 <-
-    data1 %>%    filter(str_detect(data1$variable, 'Mean') | str_detect(data1$variable, 'STD') | str_detect   (data1$variable, 'mean') | str_detect(data1$variable, 'std')) %>%
-    subset(select=c(subject_num, activity, variable , value)) %>%
-    group_by(subject_num,activity,variable) %>%
-    summarise(value=sum(value))
-data3 <- summarize(data2, mean=mean(value))
-                
+    data1 %>%    filter(str_detect(data1$measurement, 'Mean') | str_detect(data1$measurement, 'STD') | str_detect   (data1$measurement, 'mean') | str_detect(data1$measurement, 'std')) %>%
+      subset(select=c(subject_num, activity, measurement , value)) %>%
+         group_by(subject_num,activity,measurement) %>%
+            summarise(value=mean(value))
+
 
 ## Write to CSV
-write.table(data3, file = "MyData.csv",row.names=FALSE, na="",col.names=FALSE, sep=",")
-
+write.table(data2, file = "MyData.csv",row.names=FALSE, na="",col.names=TRUE, sep=",")
 
 ###############################
-##  cleanup of intermediate data
+##  cleanup of intermediate data sets
 rm(train)
 rm(train_x)
 rm(test_y)
@@ -93,5 +90,3 @@ rm(train1)
 rm(data)
 rm(data1)
 rm(data2)
-
-
